59c73052cdbd27ac07d3eb38fdbde63a83d854ae
Jader <swilor29@gmail.com>
2021-01-20 11:38:37 -0500

    cree 2 paginas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,14 +3,19 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   {
-    path: 'home',
-    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
+    path: 'films',
+    loadChildren: () => import('./pages/home/home.module').then( m => m.HomePageModule)
+  },
+   {
+    path: 'characters',
+    loadChildren: () => import('./pages/characters/characters.module').then( m => m.CharactersPageModule)
   },
   {
     path: '',
-    redirectTo: 'home',
+    redirectTo: 'films',
     pathMatch: 'full'
   },
+ 
 ];
 
 @NgModule({
